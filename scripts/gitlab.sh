ecd27e62c24cdae4d358adc4e97ee3cdf8842427
lbrealdev <lbrealdeveloper@gmail.com>
2023-10-18 20:43:37 +0200

    feat: add gitlab script improvements

--- scripts/gitlab.sh
+++ scripts/gitlab.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-GITLAB_API_URL="https://gitlab.com/api/v4/projects"
+GITLAB_API_URL="https://gitlab.com/api/v4"
 GITLAB_PROJECT_ID="$1"
 
-function check_gitlab_repository() {
-  curl -H "PRIVATE-TOKEN: ${GITLAB_AUTH_TOKEN}" -s "${GITLAB_API_URL}/${GITLAB_PROJECT_ID}" | jq -r '.owner.username'
-  curl -H "PRIVATE-TOKEN: ${GITLAB_AUTH_TOKEN}" -s "${GITLAB_API_URL}/${GITLAB_PROJECT_ID}" | jq -r '.http_url_to_repo'
-  curl -H "PRIVATE-TOKEN: ${GITLAB_AUTH_TOKEN}" -s "${GITLAB_API_URL}/${GITLAB_PROJECT_ID}" | jq .
+function gitlab_api() {
+  GITLAB_USER_URL=${GITLAB_API_URL/%/\/user}
+  GITLAB_PROJECT_URL=${GITLAB_API_URL/%/\/projects\/${GITLAB_PROJECT_ID}}  
+
+  curl -H "PRIVATE-TOKEN: ${GITLAB_AUTH_TOKEN}" -s "${GITLAB_USER_URL}" | jq -r '.username'
+  curl -H "PRIVATE-TOKEN: ${GITLAB_AUTH_TOKEN}" -s "${GITLAB_PROJECT_URL}" | jq -r '.http_url_to_repo'
+  curl -H "PRIVATE-TOKEN: ${GITLAB_AUTH_TOKEN}" -s "${GITLAB_PROJECT_URL}" | jq .
 }
 
-check_gitlab_repository
+gitlab_api
+
